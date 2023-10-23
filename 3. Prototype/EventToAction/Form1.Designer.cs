985109ee9dd92874f8a23257479f619536b3d741
sparkins01 <parkinson.sl@gmail.com>
2023-10-23 14:08:55 +0100

    Report and code update

--- 3. Prototype/EventToAction/Form1.Designer.cs
+++ 3. Prototype/EventToAction/Form1.Designer.cs
@@ -213,7 +213,7 @@ namespace EventToAction
             // button1
             // 
             button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            button1.Location = new System.Drawing.Point(841, 711);
+            button1.Location = new System.Drawing.Point(574, 707);
             button1.Name = "button1";
             button1.Size = new System.Drawing.Size(112, 34);
             button1.TabIndex = 14;
@@ -265,9 +265,9 @@ namespace EventToAction
             // textBoxDataDir
             // 
             textBoxDataDir.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
-            textBoxDataDir.Location = new System.Drawing.Point(574, 963);
+            textBoxDataDir.Location = new System.Drawing.Point(574, 615);
             textBoxDataDir.Name = "textBoxDataDir";
-            textBoxDataDir.Size = new System.Drawing.Size(1290, 31);
+            textBoxDataDir.Size = new System.Drawing.Size(1261, 31);
             textBoxDataDir.TabIndex = 16;
             textBoxDataDir.Text = "C:\\Users\\spark\\Documents\\GitHub\\ARCD\\3. Prototype\\Data";
             // 
@@ -275,7 +275,7 @@ namespace EventToAction
             // 
             label8.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
             label8.AutoSize = true;
-            label8.Location = new System.Drawing.Point(82, 963);
+            label8.Location = new System.Drawing.Point(82, 615);
             label8.Name = "label8";
             label8.Size = new System.Drawing.Size(124, 25);
             label8.TabIndex = 15;
